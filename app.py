daa11e0f8a1abbb3291b087b73de4438747802c8
lenorium <lenorium>
2022-09-26 22:58:28 +0200

    added many to many relation to meal and recipe tables

--- app.py
+++ app.py
@@ -16,19 +16,25 @@ def add_recipes():
         if not name:
             break
         description = input('Recipe description:').strip()
-        services.create_recipe(name, description)
+
+        meals = services.get_meals()
+        print(' '.join([f'{i + 1}) {m.name}' for i, m in enumerate(meals)]))
+        selected_meals_indices = input('When the dish can be served: ').strip().split()
+        selected_meals = [meals[int(i) - 1] for i in selected_meals_indices]
+
+        services.create_recipe(name, description, selected_meals)
 
 
 if __name__ == '__main__':
     parse_cli_args()
 
-    data = {"meals": ("breakfast", "brunch", "lunch", "supper"),
-            "ingredients": ("milk", "cacao", "strawberry", "blueberry", "blackberry", "sugar"),
-            "measures": ("ml", "g", "l", "cup", "tbsp", "tsp", "dsp", "")}
-
-    services.create_measures(data['measures'])
-    services.create_ingredients(data['ingredients'])
-    services.create_recipe(data['meals'])
+    # data = {"meals": ("breakfast", "brunch", "lunch", "supper"),
+    #         "ingredients": ("milk", "cacao", "strawberry", "blueberry", "blackberry", "sugar"),
+    #         "measures": ("ml", "g", "l", "cup", "tbsp", "tsp", "dsp", "")}
+    #
+    # services.create_measures(data['measures'])
+    # services.create_ingredients(data['ingredients'])
+    # services.create_meals(data['meals'])
 
     add_recipes()
 
--- services.py
+++ services.py
@@ -12,40 +12,14 @@ def create_ingredients(ingredient_names: tuple):
     repository.save_bulk(ingredients)
 
 
-def create_recipe(name, description):
-    repository.save(Recipe(name, description))
+def create_recipe(name, description, meals):
+    repository.save(Recipe(name, description, meals))
 
 
 def create_meals(meal_names: tuple):
     meals = [Meal(name) for name in meal_names]
     repository.save_bulk(meals)
 
-# class MeasureService:
-#
-#     @staticmethod
-#     def create(measure_names: tuple):
-#         measures = [Measure(name) for name in measure_names]
-#         repository.save_bulk(measures)
-#
-#
-# class IngredientService:
-#
-#     @staticmethod
-#     def create(ingredient_names: tuple):
-#         ingredients = [Ingredient(name) for name in ingredient_names]
-#         repository.save_bulk(ingredients)
-#
-#
-# class MealService:
-#
-#     @staticmethod
-#     def create(meal_names: tuple):
-#         meals = [Meal(name) for name in meal_names]
-#         repository.save_bulk(meals)
-#
-#
-# class RecipeService:
-#
-#     @staticmethod
-#     def create(name, description):
-#         repository.save(Recipe(name, description))
+
+def get_meals():
+    return repository.get_all(Meal)
